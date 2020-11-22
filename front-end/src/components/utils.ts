84e98936fc79b052894cf97abda747f0d5072146
KilMer56 <mer.killian56@gmail.com>
2020-11-22 18:40:57 +0100

    Moved Utils

--- front-end/src/components/utils.ts
+++
@@ -1,7 +0,0 @@
-
-function validEmail(email: string): boolean {
-    let re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
-}
-
-export default validEmail;
