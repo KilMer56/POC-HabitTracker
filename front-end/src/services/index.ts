1bed4603dcd9bde7b7a02bd9919111de5aced66a
KilMer56 <mer.killian56@gmail.com>
2020-11-22 21:08:55 +0100

    Add Bearer Token axios

--- front-end/src/services/index.ts
+++ front-end/src/services/index.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import UserStore from "@/store/user.store"
 
 const Axios = axios.create({
   baseURL: 'http://localhost:3000',
@@ -6,5 +7,17 @@ const Axios = axios.create({
     'Content-type': 'application/json',
   },
 });
+
+Axios.interceptors.request.use(
+  (config) => {
+    let token = UserStore.getToken();
+
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${ token }`;
+    }
+
+    return config;
+  }
+);
 
 export default Axios;
