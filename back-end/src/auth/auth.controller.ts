952d2ef2a9b33ef9c31a406bca93d6c9693d3ae7
KilMer56 <mer.killian56@gmail.com>
2020-11-22 18:42:18 +0100

    Correct interceptors errrors

--- back-end/src/auth/auth.controller.ts
+++ back-end/src/auth/auth.controller.ts
@@ -2,7 +2,6 @@ import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
 import { LocalAuthGuard } from './local/local-auth.guard';
 import { JwtAuthGuard } from './jwt/jwt-auth.guard';
 import { AuthService } from './auth.service';
-import { Response } from "../interceptors/response.interceptor"
 
 @Controller()
 export class AuthController {
--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ResponseInterceptor } from "./interceptors/response.interceptor"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
